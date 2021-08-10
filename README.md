9e59b6290fd2acef5b8ec53d4f40643231997519
Fran Canete <francanetedev@gmail.com>
2021-08-10 12:45:35 +0100

    Added error validation functions

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# techdegree-project-7
- Techdegree Project 7
+<h1 align="center">Interactive registration form ✏️</h1>
+
+<p align="left"> <a href="https://twitter.com/francanetecom" target="blank"><img src="https://img.shields.io/twitter/follow/francanetecom?logo=twitter&style=for-the-badge" alt="francanetecom" /></a> </p>
+
+<h3 align="left">Connect with me:</h3>
+<p align="left">
+<a href="https://twitter.com/francanetecom" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/twitter.svg" alt="francanetecom" height="30" width="40" /></a>
+</p>
+
+<h3 align="left">Languages and Tools:</h3>
+<p align="left"> <a href="https://www.w3schools.com/css/" target="_blank"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg" alt="css3" width="40" height="40"/> </a> <a href="https://www.w3.org/html/" target="_blank"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg" alt="html5" width="40" height="40"/> </a> <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" alt="javascript" width="40" height="40"/> </a> </p>
--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-/*      focusMethod()
+/*      focus()
  *      set the focus on the input 'name' on page load
  */
 
@@ -7,15 +7,7 @@ document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('name').focus();
 });
 
-// focusMethod = function getFocus() {
-//     document.getElementById("name").focus();
-//   }
-// focusMethod();
-
-
-/*      "Job Role" section:
- * 
- */
+/*      "Job Role" section      */
 
 const jobRole = document.getElementById('title');
 const otherJobRole = document.getElementById('other-job-role');
@@ -29,15 +21,12 @@ jobRole.addEventListener('change', e => {
     }
 });
 
-/*      "T-Shirt Info" section
- * 
- */
+/*      "T-Shirt Info" section      */
 
 
 const design = document.getElementById('design');
 const color = document.getElementById('color');
 const colorOptions = document.getElementById('color').children;
-
 color.disabled = true;
 
 
@@ -55,9 +44,8 @@ design.addEventListener('change', e => {
     }
 })
 
-/*      "Register for Activities" section
- * 
- */
+/*      "Register for Activities" section       */
+
 
 const registerForActivities = document.getElementById('activities');
 const displayTotal = document.getElementById('activities-cost');
@@ -66,9 +54,23 @@ let activities = 0;
 
 registerForActivities.addEventListener('change', e => {
     const dataCost = +e.target.getAttribute('data-cost');
+    const checkedBox = e.target;
 
-    if (e.target.checked) {
-        
+    for (let i = 0; i < checkbox.length; i++){
+
+        if (checkedBox.getAttribute('data-day-and-time') === checkbox[i].getAttribute('data-day-and-time')){
+            if(checkedBox.checked){
+                checkbox[i].disabled = true;
+                checkbox[i].parentNode.classList.add('disabled');                
+            } else if (!checkedBox.checked){
+                checkbox[i].disabled = false;
+                checkbox[i].parentNode.classList.remove('disabled');
+            }
+        }
+        checkedBox.parentElement.classList.remove('disabled');
+        checkedBox.disabled = false;
+    }
+    if (checkedBox.checked) {
         totalCost = totalCost + dataCost;
         activities = activities +1;
     } else {
@@ -76,26 +78,19 @@ registerForActivities.addEventListener('change', e => {
         activities = activities -1;
     }
     displayTotal.innerHTML = `Total: $${totalCost}`;
-    console.log(activities);
 })
 
 
 
-/*      "Payment Info" section
- * 
- */
+/*      "Payment Info" section      */
 
 const paymentMethod = document.getElementById('payment');
 const creditCard = document.getElementById('credit-card');
 const paypal = document.getElementById('paypal');
 const bitcoin = document.getElementById('bitcoin');
-
+paymentMethod[1].selected = true;
 paypal.hidden = true;
 bitcoin.hidden = true;
-
-
-// const paymentSelected = paymentMethod.children[1].setAttribute('selected', '');
-
 
 paymentMethod.addEventListener('change', e => {
     if (e.target.value === 'paypal'){
@@ -115,9 +110,7 @@ paymentMethod.addEventListener('change', e => {
 })
 
 
-/*      Form Validation
- * 
- */
+/*      Form Validation     */
 
 const userName = document.getElementById('name');
 const email = document.getElementById('email');
@@ -126,49 +119,139 @@ const zipCode = document.getElementById('zip');
 const cvv = document.getElementById('cvv');
 const form = document.querySelector('form');
 
-/*  Validation functions */
+const activitiesBox = document.getElementById('activities-box');
+
+const emailHint = document.getElementById('email-hint');
+
+/* Validator function */
+
+function errorValidator(input, test){
+    const parent = input.parentElement;
+    if(!test){
+        parent.classList.add('not-valid');
+        parent.classList.remove('valid');
+        parent.lastElementChild.style.display = 'block';
+    }
+    if(test){
+        parent.classList.add('valid');
+        parent.classList.remove('not-valid');
+        parent.lastElementChild.style.display = 'none';
+    }
+    return test;
+}
+
+/*  Field Validations */
 
 const nameValitation = () => {
     const userNameValue = userName.value;
-    return /^[a-zA-Z ]+$/.test(userNameValue);
+    const userNameTest = /^[a-zA-Z ]+$/.test(userNameValue);
+    const test = errorValidator(userName, userNameTest); 
+    return test;
 }
 
 const emailValidation = () => {
     const emailValue = email.value;
-    return /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.com$/i.test(emailValue);
+    const emailTest = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.com$/i.test(emailValue);
+    const test = errorValidator(email, emailTest);
+    return test;
   }
 
 const activitiesValidation = () => {
     const activitiesChecked = activities >= 1;
-    return activitiesChecked; 
+    const test = errorValidator(activitiesBox, activitiesChecked);
+    return test;
 }
 
 const cardNumberValidation = () => {
     const cardNumberValue = cardNumber.value;
-    return /^\d{13,16}$/.test(cardNumberValue);
+    const cardNumberTest = /^\d{13,16}$/.test(cardNumberValue);
+    const test = errorValidator(cardNumber, cardNumberTest);
+    return test;
 } 
 
 const zipValidation = () => {
     const zipValue = zipCode.value;
-    return /^\d{5}$/.test(zipValue);
+    const zipTest = /^\d{5}$/.test(zipValue);
+    const test = errorValidator(zipCode, zipTest);
+    return test;
 }
 
 const cvvValidator = () => {
     const cvvValue = cvv.value;
-    return /^\d{3}$/.test(cvvValue);
+    cvvTest = /^\d{3}$/.test(cvvValue);
+    const test = errorValidator(cvv, cvvTest);
+    return test;
 }
 
-
 form.addEventListener('submit', e => {
+    nameValitation();
+    emailValidation();
+    activitiesValidation();
+    cardNumberValidation();
+    zipValidation();
+    cvvValidator();
+
     if ( nameValitation() && emailValidation() && activitiesValidation() ){
 
-    } else if (paymentMethod.value === 'credit-card'){
+    } else {
+        if(email.value === ''){
+            emailHint.style.display = 'block';
+            emailHint.innerHTML = 'Email field cannot be blank';
+        }
+        e.preventDefault();
+    }
+    if (paymentMethod.value === 'credit-card'){
         if (cardNumberValidation() && zipValidation() && cvvValidator() ){
+
         } else {
             e.preventDefault()
         }
-    } else {
-        e.preventDefault();
+    } 
+})
+
+/*      Real-time error message     */
+
+userName.addEventListener('keyup', e => {
+    if (userName.value){
+        nameValitation();
     }
 })
+
+email.addEventListener('keyup', e => {
+    if(email.value === ''){
+        emailHint.style.display = 'block';
+        emailHint.innerHTML = 'Email field cannot be blank';
+    } else if (!emailValidation()){
+        emailHint.innerHTML = 'Email address must be formatted correctly';
+    }
+    emailValidation();
+})
+
+cardNumber.addEventListener('keyup', e => {
+    cardNumberValidation();
+})
+
+zipCode.addEventListener('keyup', e => {
+    zipValidation();
+})
+
+cvv.addEventListener('keyup', e => {
+    cvvValidator();
+})
+
+
+/*      Accessibility       */
+
+const checkbox = document.querySelectorAll('input[type="checkbox"]');
+
+for (let i = 0; i < checkbox.length; i++){
+
+    checkbox[i].addEventListener('focus', e => {
+        e.target.parentElement.classList.add('focus');
+    })
+
+    checkbox[i].addEventListener('blur', e => {
+        e.target.parentElement.classList.remove('focus');
+    })
+}
 
